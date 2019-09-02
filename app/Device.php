ce80fdbe04984b07303a875b47db03be328cee9b
Cosmin Anghel <cosminzorr@gmail.com>
2019-09-02 22:42:41 +0300

    Update Device.php

--- app/Device.php
+++ app/Device.php
@@ -1,6 +1,7 @@
 <?php namespace App;
 class Device {
     const DEFAULT_KEY = 'a3K8Bx%2r8Y7#xDh';
+    const AC_IP = '192.168.0.139';
 
     public $mac;
     public $mid;
@@ -11,159 +12,97 @@ class Device {
 
     public function pair()
     {
-        $sPack = '{
-  "mac": "'.$this->mac.'",
-  "t": "bind",
-  "uid": 0
-}';
+        $sPack = '{ "mac": "'.$this->mac.'", "t": "bind", "uid": 0 }';
         $sEncPack = $this->fnEncrypt($sPack, self::DEFAULT_KEY);
-        $sRequest = '{
-  "cid": "'.$this->cid.'",
-  "i": 1,
-  "pack": "'.$sEncPack.'",
-  "t": "pack",
-  "tcid": "app",
-  "uid": 0
-}';
-//        echo 'sending data: ' . $sRequest.PHP_EOL;
-        $fp = fsockopen("udp://192.168.1.109", 7000, $errno, $errstr);
-        fwrite($fp, $sRequest);
+        $request = '{
+            "cid": "' . $this->cid . '",
+            "i": 1,
+            "pack": "' . $sEncPack . '",
+            "t": "pack",
+            "tcid": "app",
+            "uid": 0
+        }';
+
+        $this->sendData($request);
+    }
+
+    private function generateRequest($pack) {
+        $encryptedPack = $this->fnEncrypt($pack, $this->key);
+        $request = '{
+            "t": "pack",
+            "i": 0,
+            "uid": 0,
+            "cid": "' . $this->mac . '",
+            "tcid": "",
+            "pack": "'. $encryptedPack . '"
+        }';
+        return $request;
+    }
+
+    private function sendData($request) {
+        $fp = fsockopen("udp://" . self::AC_IP, 7000, $errno, $errstr);
+        fwrite($fp, $request);
         $x = fread($fp, 1024);
+    
         if ($x) {
             $oData = json_decode($x);
             $oResponse = json_decode($this->fnDecrypt($oData->pack, self::DEFAULT_KEY));
-            $this->key = $oResponse->key;
+            if (is_object($oResponse)) {
+                $this->key = $oResponse->key;
+            }
             fclose($fp);
         }
     }
 
-    public function on()
-    {
-        $sPack = '{
-  "opt": ["Pow"],
-  "p": [1],
-  "t": "cmd"
-}';
-        $sEncPack = $this->fnEncrypt($sPack, $this->key);
-        $sRequest = '{
-  "t": "pack",
-  "i": 0,
-  "uid": 0,
-  "cid": "'.$this->mac.'",
-  "tcid": "",
-  "pack": "'.$sEncPack.'"
-}';
-//        echo 'sending data: ' . $sRequest.PHP_EOL;
-        $fp = fsockopen("udp://192.168.1.109", 7000, $errno, $errstr);
-        fwrite($fp, $sRequest);
-        $x = fread($fp, 1024);
-        var_dump($x);exit;
-        if ($x) {
-            var_dump($x);exit;
-            $oData = json_decode($x);
-            $oResponse = json_decode($this->fnDecrypt($oData->pack, self::DEFAULT_KEY));
-            $this->key = $oResponse->key;
-            fclose($fp);
-        }
+    private function executeCommand($pack) {
+        $request = $this->generateRequest($pack);
+        $this->sendData($request);
     }
 
-    public function setSwing($sValue)
-    {
-        $sPack = '{
-  "opt": ["SwUpDn"],
-  "p": ['.$sValue.'],
-  "t": "cmd"
-}';
-        $sEncPack = $this->fnEncrypt($sPack, $this->key);
-        $sRequest = '{
-  "t": "pack",
-  "i": 0,
-  "uid": 0,
-  "cid": "'.$this->mac.'",
-  "tcid": "",
-  "pack": "'.$sEncPack.'"
-}';
-//        echo 'sending data: ' . $sRequest.PHP_EOL;
-        $fp = fsockopen("udp://192.168.1.109", 7000, $errno, $errstr);
-        fwrite($fp, $sRequest);
-        $x = fread($fp, 1024);
-        var_dump($x);exit;
-        if ($x) {
-            var_dump($x);exit;
-            $oData = json_decode($x);
-            $oResponse = json_decode($this->fnDecrypt($oData->pack, self::DEFAULT_KEY));
-            $this->key = $oResponse->key;
-            fclose($fp);
-        }
+    public function on() {
+        $pack = '{ "opt": ["Pow"], "p": [1], "t": "cmd" }';
+        $this->executeCommand($pack);
     }
 
-    public function setTemp($sTemp)
-    {
-        $sPack = '{
-  "opt": ["TemUn", "SetTem"],
-  "p": [0, '.$sTemp.'],
-  "t": "cmd"
-}';
-        $sEncPack = $this->fnEncrypt($sPack, $this->key);
-        $sRequest = '{
-  "t": "pack",
-  "i": 0,
-  "uid": 0,
-  "cid": "'.$this->mac.'",
-  "tcid": "",
-  "pack": "'.$sEncPack.'"
-}';
-//        echo 'sending data: ' . $sRequest.PHP_EOL;
-        $fp = fsockopen("udp://192.168.1.109", 7000, $errno, $errstr);
-        fwrite($fp, $sRequest);
-        $x = fread($fp, 1024);
-        var_dump($x);exit;
-        if ($x) {
-            var_dump($x);exit;
-            $oData = json_decode($x);
-            $oResponse = json_decode($this->fnDecrypt($oData->pack, self::DEFAULT_KEY));
-            $this->key = $oResponse->key;
-            fclose($fp);
-        }
+    public function off() {
+        $pack = '{ "opt": ["Pow"], "p": [0], "t": "cmd" }';
+        $this->executeCommand($pack);
     }
 
-    public function off()
-    {
-        $sPack = '{
-  "opt": ["Pow"],
-  "p": [0],
-  "t": "cmd"
-}';
-        $sEncPack = $this->fnEncrypt($sPack, $this->key);
-        $sRequest = '{
-  "t": "pack",
-  "i": 0,
-  "uid": 0,
-  "cid": "'.$this->mac.'",
-  "tcid": "",
-  "pack": "'.$sEncPack.'"
-}';
-//        echo 'sending data: ' . $sRequest.PHP_EOL;
-        $fp = fsockopen("udp://192.168.1.109", 7000, $errno, $errstr);
-        fwrite($fp, $sRequest);
-        $x = fread($fp, 1024);
-        var_dump($x);exit;
-        if ($x) {
-            var_dump($x);exit;
-            $oData = json_decode($x);
-            $oResponse = json_decode($this->fnDecrypt($oData->pack, self::DEFAULT_KEY));
-            $this->key = $oResponse->key;
-            fclose($fp);
-        }
+    public function setSwing($value) {
+        // default: 0,
+        // full: 1 - swing in full range
+        // fixedTop: 2 - fixed in the upmost position (1/5)
+        // fixedMidTop: 3 - fixed in the middle-up position (2/5)
+        // fixedMid: 4 - fixed in the middle position (3/5)
+        // fixedMidBottom: 5 - fixed in the middle-low position (4/5)
+        // fixedBottom: 6 - fixed in the lowest position (5/5)
+        // swingBottom: 7 - swing in the downmost region (5/5)
+        // swingMidBottom: 8 - swing in the middle-low region (4/5)
+        // swingMid: 9 - swing in the middle region (3/5)
+        // swingMidTop: 10 - swing in the middle-up region (2/5)
+        // swingTop: 11 - swing in the upmost region (1/5)
+        
+        $pack = '{ "opt": ["SwUpDn"], "p": [' . $value . '], "t": "cmd" }';
+        $this->executeCommand($pack);
     }
 
-    function fnEncrypt($sValue, $sSecretKey, $sMethod = 'aes-128-ecb')
-    {
+    public function setTemperature($value) {
+        $pack = '{ "opt": ["TemUn", "SetTem"], "p": [0, ' . $value . '], "t": "cmd" }';
+        $this->executeCommand($pack);
+    }
+
+    public function setFanSpeed($value) {
+        // value - 0 (auto), 1 (low), 2 (medium - low), 3 (medium), 4 (medium - high), 5 (high)
+        $pack = '{ "opt": ["WdSpd"], "p": [' . $value . '], "t": "cmd" }';
+        $this->executeCommand($pack);
+    }
+
+    function fnEncrypt($sValue, $sSecretKey, $sMethod = 'aes-128-ecb') {
         return base64_encode(openssl_encrypt($sValue, $sMethod, $sSecretKey, OPENSSL_RAW_DATA));
     }
 
-    function fnDecrypt($sValue, $sSecretKey, $sMethod = 'aes-128-ecb')
-    {
+    function fnDecrypt($sValue, $sSecretKey, $sMethod = 'aes-128-ecb') {
         $sText = base64_decode($sValue);
         return openssl_decrypt($sText, $sMethod, $sSecretKey, OPENSSL_RAW_DATA);
     }
